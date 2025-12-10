a3c0d68c43ec8467ad40cbb68072a843a0d4f4a0
Robots Montagnards <robotsmontagnards@gmail.com>
2025-12-10 17:27:50 -0500

    code sorks on swerve (direction)

--- src/main/java/frc/robot/components/Motor.java
+++ src/main/java/frc/robot/components/Motor.java
@@ -27,7 +27,7 @@ public class Motor {
 
     public Motor()
     {
-        motor = new SparkMax(8, SparkLowLevel.MotorType.kBrushless);
+        motor = new SparkMax(7, SparkLowLevel.MotorType.kBrushless);
         encoder = motor.getEncoder();
     }
 
